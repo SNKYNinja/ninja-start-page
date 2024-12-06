95c70c523465edea3818c37908cf04d63375e05c
Aditya <aditya.chaudhary0523@gmail.com>
2024-12-06 21:58:47 +0530

    feat: add custom config

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -11,7 +11,7 @@ const Meta = () => {
 
 	useEffect(() => {
 		// Set title
-		setTitle(settings.username + " Start Page")
+		setTitle("Welcome")
 
 		// Return if there is no icon
 		if (!settings.fetch.image) return
